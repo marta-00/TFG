8267b4faa21e4b7f1ebd614c74c455b57b75abe1
marta-00 <mrf9762@gmail.com>
2025-02-24 18:43:48 +0100

    modified:   __pycache__/magnitudes.cpython-313.pyc 	new file:   bici.py 	renamed:    datos_indv.py -> carreras.py 	new file:   datos_bici/Bici_Half_Santander.gpx 	new file:   datos_bici/Bici_TRI.gpx 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (1).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (2).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (3).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (4).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (5).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (6).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (7).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana (8).gpx" 	new file:   "datos_bici/Bicicleta_por_la_ma\303\261ana.gpx" 	new file:   "datos_bici/Bike_Triatl\303\263n_Ciudad_Santander_2024.gpx" 	new file:   "datos_bici/Ciclismo_40km_Triatl\303\263n_Santander_ol\303\255mpico_.gpx" 	new file:   "datos_bici/Ciclismo_Triatl\303\263n_Half_Santander.gpx" 	new file:   datos_bici/Half_Santander_sector_bici.gpx 	new file:   datos_bici/Morning_Ride (1).gpx 	new file:   datos_bici/Morning_Ride (2).gpx 	new file:   datos_bici/Morning_Ride (3).gpx 	new file:   datos_bici/Morning_Ride (4).gpx 	new file:   datos_bici/Morning_Ride (5).gpx 	new file:   datos_bici/Morning_Ride (6).gpx 	new file:   datos_bici/Morning_Ride (7).gpx 	new file:   datos_bici/Morning_Ride.gpx 	new file:   datos_bici/Santander_Triathlon_bike.gpx 	new file:   datos_bici/Triatlon_Ciudad_de_Santander_Ciclismo.gpx 	new file:   datos_bici/Triatlon_Ciudad_de_Santander_S2.gpx 	new file:   datos_bici/Triatlon_de_Santander_2024_Ride.gpx 	new file:   "datos_bici/Triatl\303\263n_Ciudad_de_Santander_Ride.gpx" 	new file:   "datos_bici/Triatl\303\263n_Santander_2024_p12_GGEE.gpx" 	new file:   "datos_bici/Triatl\303\263n_de_Santander_.gpx" 	new file:   "datos_bici/Triatl\303\263n_de_Santander_2024_Bicicleta_.gpx" 	new file:   "datos_bici/Un_m\303\255tico_.gpx" 	new file:   datos_bici/_Santander_24.gpx 	deleted:    datos_total.py

--- carreras.py
+++ carreras.py
@@ -1,18 +1,20 @@
 
 from leer_datos import leer_datos_gpx
 from magnitudes import *
+import matplotlib.pyplot as plt
 
 def una_carrera():
     #leer_datos_csv('datos/Carrera_de_mañana.gpx.csv')
     coord = leer_datos_gpx('datos/Carrera_de_mañana(8).gpx')
     dist_tramo, dist_total = distancia(coord)
+
     # calcular velocidad instantánea
     velocidades = velocidad(dist_tramo)
     velocidades = list(set(velocidades)-set(detectar_atipicos_zscore(velocidades)))
     crear_histogramas(velocidades, nombre1 = "velocidades")
 
     # separar datos
-    coord_pares, coord_impares = separar_datos(coord, 2)
+    coord_pares, coord_impares = separar_datos(coord, 5)
 
     # obtener distancias_tramo
     dist_tramo, dist_total = distancia(coord)
@@ -33,6 +35,55 @@ def una_carrera():
     dist_tramo_impar = list(set(dist_tramo_impar)-set(detectar_atipicos_zscore(dist_tramo_impar)))
 
     crear_histogramas(dist_tramo, dist_tramo_par, dist_tramo_impar, "total", "pares", "impares")
+    print(f"distancia total: {dist_total}")
+    print(f"distancia total pares: {dist_total_par}")
+    print(f"distancia total impares: {dist_total_impar}")
+    plt.show()
+
+def total_carreras():
+    # leer archivo magnitudes.csv
+    import pandas as pd
+    magnitudes = pd.read_csv('magnitudes.csv', delimiter=',', encoding='latin1')
+    #print(magnitudes)
+
+    # # crear histograma con las distancias medias
+    # import matplotlib.pyplot as plt
+    # distancias = magnitudes['Distancia_total(m)']
+    # plt.hist(distancias, bins=len(distancias), color='blue', edgecolor='blue', alpha=0.7)
+
+    # # añadir en rojo una recta en el valor 21.097
+    # plt.axvline(x=21097, color='red', linestyle='--')
+
+    # plt.title("Histograma - distancias medias")
+    # plt.xlabel("Distancia total (m)")
+    # plt.ylabel("Número de carreras")
+    # plt.show()
+
+    # # crear histograma con las altitudes
+    # altitudes = magnitudes['Altitud(m)']
+    # plt.hist(altitudes, bins=len(altitudes), color='green', edgecolor='green', alpha=0.7)
+    # plt.title("Histograma - altitudes")
+    # plt.xlabel("Altitud (m)")
+    # plt.ylabel("Número de carreras")
+    # plt.show()
+
+    ## comparar distancias
+    # obtener distancias
+    distancias = magnitudes['Distancia_total(m)']
+    distancias_par = magnitudes['Distancia_par(m)']
+    distancias_impar = magnitudes['Distancia_impar(m)']
+
+    #calcular error absoluto, dato exacto 21097,5 m
+    error_total = abs(distancias - 21097.5)
+    error_par = abs(distancias_par - 21097.5)
+    error_impar = abs(distancias_impar - 21097.5)
+
+    for i in range(len(error_total)):
+        nombre_carrera = magnitudes['Nombre'][i]
+        if error_total[i] > error_par[i] and error_total[i] > error_impar[i]:
+            print(f"La medida mejora")
+        else:
+            print(f"la medida empeora para la carrera: {nombre_carrera}")
 
 def main():
     # crear archivo csv para almacenar datos
@@ -99,5 +150,5 @@ def main():
     magnitudes.close()
 
 if __name__ == "__main__":
-   main()
-
+   
+    una_carrera()
