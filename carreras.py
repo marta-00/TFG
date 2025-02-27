c7ebb264c9ed3a893e871cff2bf2ef8ebaf7537e
marta-00 <mrf9762@gmail.com>
2025-02-27 13:09:22 +0100

    modified:   carreras.py

--- carreras.py
+++ carreras.py
@@ -86,7 +86,12 @@ def total_carreras():
         else:
             print(f"la medida empeora para la carrera: {nombre_carrera}")
 
-def main():
+def graficos_total_carreras():
+    """
+    Función que crea un archivo CSV y guarda los datos de la distancia total, altitud y velocidad de cada 
+    carrera. 
+    También guarda todos los gráficos (recorrido, velocidad y distancia) en una carpeta con el nombre de la carrera
+    """
     # crear archivo csv para almacenar datos
     import os
     import matplotlib.pyplot as plt
@@ -94,7 +99,6 @@ def main():
     magnitudes.write(f"Nombre,Distancia_total(m),Altitud(m),Velocidad(m/s)\n")
 
     # bucle que recorre todos los archivos GPX de la carpeta datos y los abre con la función leer_datos_gpx
-    
     for nombre_archivo in os.listdir('datos'):
         if nombre_archivo.endswith('.gpx'):
             # leer archivo gpx
@@ -108,42 +112,79 @@ def main():
             # calcular magnitudes
             dist_tramo,dist_total = distancia(coord)
             alt = altitud(coord)
-
+            
+            ## RECORRIDO
             graf = grafico(coord)
             plt.savefig(f'{carpeta}/recorrido.png')
             plt.close(graf)
 
-            # ## DISTANCIAS TRAMO
-            # #separar datos
-            # coord_pares, coord_impares = separar_datos(coord, 2)
+            ## DISTANCIAS TRAMO
+            #separar datos
+            coord_pares, coord_impares = separar_datos(coord, 2)
+            #obtener distancias_tramo
+            dist_tramo, dist_total = distancia(coord)
+            dist_tramo_par, dist_total_par = distancia(coord_pares)
+            dist_tramo_impar, dist_total_impar = distancia(coord_impares)
+            #crear histogramas
+            dist_td = crear_histogramas(dist_total, "distancias")
+            plt.savefig(f'{carpeta}/distancias_total_datos.png')
+            plt.close(dist_td)
+            imagen = crear_histogramas(dist_tramo_par, dist_tramo_impar, "pares", "impares")
+            # añadir a carpeta 
+            plt.savefig(f'{carpeta}/distancias_mitad_datos.png')
+            plt.close(imagen)
 
-            # #obtener distancias_tramo
-            # dist_tramo, dist_total = distancia(coord)
-            # dist_tramo_par, dist_total_par = distancia(coord_pares)
-            # dist_tramo_impar, dist_total_impar = distancia(coord_impares)
+            # VELOCIDADES
+            # calcular velocidad instantánea
+            velocidades = velocidad(dist_tramo)
+            figura = crear_histogramas(velocidades, nombre1 = "velocidades")
+            #añadir a carpeta histogramas_velocidad
+            plt.savefig(f'{carpeta}/velocidades.png')
+            plt.close(figura)
 
-            # #crear histogramas
-            # imagen = crear_histogramas(dist_tramo, dist_tramo_par, dist_tramo_impar, "total", "pares", "impares")
-
-            # # añadir a carpeta 
-            # plt.savefig(f'{carpeta}/distancias_tramo.png')
-            # plt.close(imagen)
-
-            # # VELOCIDADES
-            # # calcular velocidad instantánea
-            # velocidades = velocidad(dist_tramo)
-            # figura = crear_histogramas(velocidades, nombre1 = "velocidades")
-
-            # #añadir a carpeta histogramas_velocidad
-            # plt.savefig(f'{carpeta}/velocidades.png')
-            # plt.close(figura)
-
-            # # MAGNITUDES 
-            # # añadir magnitudes al archivo csv
+            # MAGNITUDES 
+            # añadir magnitudes al archivo csv
             magnitudes.write(f"{nombre_archivo},{dist_total},{alt},{0}\n")
             
     magnitudes.close()
 
-if __name__ == "__main__":
-   
-    main()
+def comparación_distancias():
+    """
+    Función que separa los datos separados n veces y crea un gráfico comparando cómo varía la 
+    distancia total de la carrera en función de n. 
+    En un mismo gráfico se incluyen 3 carreras distintas para poder compararlas.
+    """ 
+    coord1 = leer_datos_gpx('datos/Carrera_de_mañana(1).gpx')
+    coord2 = leer_datos_gpx('datos/Carrera_de_mañana(5).gpx')
+    coord3 = leer_datos_gpx('datos/Carrera_de_mañana(8).gpx')
+    datos = [coord1, coord2, coord3]
+
+    #crear figura 
+    plt.figure(figsize=(10, 6))
+
+    for i,coord in enumerate(datos):
+        distancias_totales = []
+        dist_tramo, dist_total = distancia(coord)
+        distancias_totales.append(dist_total)
+        for n in range(2, 20, 1):
+            dat1, dat2 = separar_datos(coord, n)
+            # calcular distancias
+            dist_1, dist_total2 = distancia(dat1)
+            distancias_totales.append(dist_total2)
+
+        #añadir datos a la figura
+        plt.plot(range(1, len(distancias_totales) + 1), distancias_totales, marker='o')
+    # Configurar el gráfico
+    distancia_media_maraton = 21097.5  # Distancia en metros
+    plt.axhline(y=distancia_media_maraton, color='red', linestyle='--', label='Media Maratón (21.0975 km)')
+
+    plt.title('Comparación de Distancias Totales en Función de n')
+    plt.xlabel('Número de Separaciones (n)')
+    plt.ylabel('Distancia Total (m)')
+    plt.xticks(ticks=range(1, len(distancias_totales) + 1))
+    plt.legend(['Carrera_de_mañana(1)', 'Carrera_de_mañana(5)', 'Carrera_de_mañana(8)'])
+    plt.grid()
+    plt.show()
+
+comparación_distancias()   
+
